ad376232d024645f89b7c465520a3e942aa33b96
heyuanlong <1023417614@qq.com>
2017-06-18 23:09:05 +0800

    sdfsdfs

--- script/start.sh
+++ script/start.sh
@@ -1,2 +1,41 @@
 #!/bin/bash
 
+
+bin_path=./
+
+function startclustermanager() {
+	${bin_path}skynet ${bin_path}config/clustermanager_config
+}
+function startlogin() {
+	${bin_path}skynet ${bin_path}config/login_config
+}
+function startlobby1() {
+	${bin_path}skynet ${bin_path}config/lobby1_config
+}
+function starttrans1() {
+	${bin_path}skynet ${bin_path}config/trans1_config
+}
+
+case $1 in
+	all )
+		startclustermanager
+		startlogin
+		startlobby1
+		starttrans1
+		;;
+	clustermanager )
+		startclustermanager
+		;;
+	login )
+		startlogin
+		;;
+	lobby1 )
+		startlobby1
+		;;
+	trans1 )
+		starttrans1
+		;;
+	* )
+		echo "invalid arg"
+		;;
+esac
